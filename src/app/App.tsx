b2bb23c76102f3d04e46a76c1eef62cfc6203379
krantni <krantni@gmail.com>
2019-04-09 23:16:59 -0500

    adding icons

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,13 @@ import * as React from 'react';
 import Header from '../components/Header';
 import Experience from '../components/Experience';
 import Education from '../components/Education';
+import burgerIcon from '../images/icons/burger.svg';
+import resumeIcon from '../images/icons/resume.svg';
+import laptopIcon from '../images/icons/laptop.svg';
+import linkedInIcon from '../images/icons/linkedin.svg';
+import twitterIcon from '../images/icons/twitter.svg';
+import githubIcon from '../images/icons/github.svg';
+import links from '../constants/links';
 
 import styles from './App.module.css';
 
@@ -12,6 +19,33 @@ class App extends React.Component {
         <div className={styles.header}>
           <Header />
         </div>
+        <div className={styles.sectionIcons}>
+          <div className={styles.iconHolder}>
+            <img className={styles.icon} src={laptopIcon} alt="Projects" />
+            <div className={styles.iconText}>Projects</div>
+          </div>
+          <div className={styles.iconHolder}>
+            <img className={styles.icon} src={resumeIcon} alt="Resume" />
+            <div className={styles.iconText}>Resume</div>
+          </div>
+          <div className={styles.iconHolder}>
+            <img className={styles.icon} src={burgerIcon} alt="Burgers" />
+            <div className={styles.iconText}>Burgers</div>
+          </div>
+          <div className={styles.iconHolder} onClick={() => window.open(links.linkedIn)}>
+            <img className={styles.icon} src={linkedInIcon} alt="LinkedIn Icon" />
+            <div className={styles.iconText}>LinkedIn</div>
+          </div>
+          <div className={styles.iconHolder} onClick={() => window.open(links.github)}>
+            <img className={styles.icon} src={githubIcon} alt="GitHub" />
+            <div className={styles.iconText}>GitHub</div>
+          </div>
+          <div className={styles.iconHolder} onClick={() => window.open(links.twitter)}>
+            <img className={styles.icon} src={twitterIcon} alt="Twitter" />
+            <div className={styles.iconText}>Twitter</div>
+          </div>
+        </div>
+
         <div className={styles.experience}>
           <Experience />
         </div>
---
+++ src/constants/links.ts
@@ -0,0 +1,13 @@
+const links: URLS = {
+  linkedIn: 'https://twitter.com/nick__krantz',
+  github: 'https://github.com/krantni',
+  twitter: 'https://www.linkedin.com/in/nicholaskrantz/',
+};
+
+export interface URLS {
+  linkedIn: string;
+  github: string;
+  twitter: string;
+}
+
+export default links;
