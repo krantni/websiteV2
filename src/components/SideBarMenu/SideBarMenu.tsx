7e3456accf6ab9f35d23a7ea2aa4dc01f0284771
krantni <krantni@gmail.com>
2019-05-09 21:22:46 -0500

    sidebar styling

--- src/components/SideBarMenu/SideBarMenu.tsx
+++ src/components/SideBarMenu/SideBarMenu.tsx
@@ -20,26 +20,32 @@ const SideBarMenu = ({ openSection }: Props) => {
       <div className={styles.iconHolder} onClick={() => openSection('projects')}>
         <img className={styles.icon} src={codeIcon} alt="Projects" />
         <div className={styles.iconText}>Projects</div>
+        <span className={styles.decorativeLine} />
       </div>
       <div className={styles.iconHolder} onClick={() => openSection('resume')}>
         <img className={styles.icon} src={resumeIcon} alt="Resume" />
         <div className={styles.iconText}>Resume</div>
+        <span className={styles.decorativeLine} />
       </div>
       <div className={styles.iconHolder} onClick={() => openSection('burgers')}>
         <img className={styles.icon} src={burgerIcon} alt="Burgers" />
         <div className={styles.iconText}>Burgers</div>
+        <span className={styles.decorativeLine} />
       </div>
       <div className={styles.iconHolder} onClick={() => window.open(links.linkedIn)}>
         <img className={styles.icon} src={linkedInIcon} alt="LinkedIn Icon" />
         <div className={styles.iconText}>LinkedIn</div>
+        <span className={styles.decorativeLine} />
       </div>
       <div className={styles.iconHolder} onClick={() => window.open(links.github)}>
         <img className={styles.icon} src={githubIcon} alt="GitHub" />
         <div className={styles.iconText}>GitHub</div>
+        <span className={styles.decorativeLine} />
       </div>
       <div className={styles.iconHolder} onClick={() => window.open(links.twitter)}>
         <img className={styles.icon} src={twitterIcon} alt="Twitter" />
         <div className={styles.iconText}>Twitter</div>
+        <span className={styles.decorativeLine} />
       </div>
     </div>
   );
